2ab2f3c3f441ca997215f3849c9d4859616fbdf9
Anna Casper <anna.casper3@gmail.com>
2015-08-17 11:50:53 -0600

    new and index page are wired up

--- routes/lists.js
+++ routes/lists.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var script = require('../lib/script.js');
+var validations = require('../lib/validations.js')
 
+router.get('/', function(req, res, next){
+  res.render('lists/index')
+});
 
-router.get('/', function(req, res, next) {
+router.get('/show', function(req, res, next) {
   res.render('lists/show');
 });
+
+router.get('/new', function(req, res, next){
+  res.render('lists/new')
+});
+
+router.post("/new", function(req, res, next){
+  var errors = validations.validateNew(req.body.title, req.body.item);
+})
+
 
 module.exports = router;
