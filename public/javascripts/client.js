f24cb520168d69bd943aeed151bfa71ac0cfb1aa
Anna Casper <anna.casper3@gmail.com>
2015-08-18 12:27:15 -0600

    user can add additional items in list

---
+++ public/javascripts/client.js
@@ -0,0 +1,11 @@
+
+var add = document.getElementsByClassName('additional')[0];
+
+add.addEventListener('click', function () {
+  var input = document.createElement('input')
+  var p = document.createElement('p')
+  input.name = 'item'
+  input.placeholder = 'Item'
+  document.getElementById('extra').appendChild(input)
+  document.getElementById('extra').appendChild(p)
+}); 
