1b5e200c6ceb035d81805391d3752c9278bae3b3
Anna Casper <anna.casper3@gmail.com>
2015-08-18 11:35:32 -0600

    user can see their lists

--- routes/lists.js
+++ routes/lists.js
@@ -12,7 +12,6 @@ router.get('/', function(req, res, next){
 
 router.get('/show', function(req, res, next) {
   if(req.session.currentUser){
-    console.log(req.session.currentUser);
     res.render('lists/show');
   } else {
     res.redirect('/');
@@ -30,10 +29,16 @@ router.get('/new', function(req, res, next){
 router.post("/new", function(req, res, next){
   validations.newList(req.body.title, req.body.item, req.session.currentUser).then(function (data) {
     if(data === 'success'){
-      res.redirect('/lists/index')
+      res.redirect('/lists')
     } else {
       res.render('lists/new', {errorMessage: 'Please fix the errors listed below:', errors: data})
     }
+  })
+});
+
+router.get('/showAll', function (req, res, next) {
+  validations.showAll(req.session.currentUser).then(function (lists) {
+    res.render('lists/showAll', {lists: lists})
   })
 })
 
