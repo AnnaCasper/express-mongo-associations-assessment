b28145c122ece24fe331cb266e898a10ced0dc78
Anna Casper <anna.casper3@gmail.com>
2015-08-18 14:29:15 -0600

    fixed client side

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,10 +1,10 @@
-var add = document.getElementsByClassName('additional')[0];
-
-add.addEventListener('click', function () {
-  var input = document.createElement('input')
-  var p = document.createElement('p')
-  input.name = 'item'
-  input.placeholder = 'Item'
-  document.getElementById('extra').appendChild(input)
-  document.getElementById('extra').appendChild(p)
-});
+// var add = document.getElementsByClassName('additional')[0];
+//
+// add.addEventListener('click', function () {
+//   var input = document.createElement('input')
+//   var p = document.createElement('p')
+//   input.name = 'item'
+//   input.placeholder = 'Item'
+//   document.getElementById('extra').appendChild(input)
+//   document.getElementById('extra').appendChild(p)
+// });
